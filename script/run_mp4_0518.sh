1aa1965b884830f091470b8fe83e04b2dd9afaaa
davidkylee <davidyun0920@gmail.com>
2021-07-08 13:34:33 +0800

    feat: add a script for inference batch mp4 video

---
+++ script/run_mp4_0518.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+conda activate p37_yolov5
+
+LIBDIR=/home/openposeadmin/davidkylee/PycharmProjects/yolov5/yolov5
+export PYTHONPATH=$LIBDIR:$PYTHONPATH
+
+VIDEO_DIR=/datadrive/data-yilanvideo
+WORKDIR=/datadrive/davidkylee
+PROJECT=${WORKDIR}/HDyolov5
+LOGDIR=${WORKDIR}/log
+WEIGHT=yolov5s.pt # yolov5m.pt / yolov5l.pt / yolov5x.pt
+
+mkdir -p ${WORKDIR}
+mkdir -p ${PROJECT}
+mkdir -p ${LOGDIR}
+cd ${WORKDIR}
+for s in ${VIDEO_DIR}/*.mp4
+do
+python3 -u -m detect --source $s --weights ${WEIGHT} --project ${PROJECT} --name `basename $s` --persononly --skipfps 0 &> ${LOGDIR}/`basename $s`.log
+done
