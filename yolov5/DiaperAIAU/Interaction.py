de6f1de28daea5f41b1859747b5abe61ec84e208
davidkylee <davidyun0920@gmail.com>
2021-07-16 11:29:11 +0800

    feat: 改善interaction AIAU

--- yolov5/DiaperAIAU/Interaction.py
+++ yolov5/DiaperAIAU/Interaction.py
@@ -2,14 +2,14 @@ import os
 
 
 class posture(object):
-    def __init__(self, diaperworkarea, personworkarea, filterarea = None):
+    def __init__(self, diaperworkarea, personworkarea, filterarea=None):
         self.diaperworkarea = diaperworkarea
         self.personworkarea = personworkarea
         self.filterarea = filterarea
 
     def getpost(self, activethings):
         if self.filterarea is not None:
-            activethings = [thing for thing in activethings if thingisinarea(self.filterarea,thing['xyxy']) ]
+            activethings = [thing for thing in activethings if thingisinarea(self.filterarea, thing['xyxy'])]
         post = {}
         persons = [thing for thing in activethings if thing['label'] == 'person']
         diapers = [thing for thing in activethings if thing['label'] == 'diaper']
@@ -17,6 +17,8 @@ class posture(object):
                              thing['label'] == 'person' and thingisinarea(self.personworkarea, thing['xyxy'])]
         inworkareadiapers = [thing for thing in activethings if
                              thing['label'] == 'diaper' and thingisinarea(self.diaperworkarea, thing['xyxy'])]
+        inpersonworkareadiapers = [thing for thing in activethings if
+                                   thing['label'] == 'diaper' and thingisinarea(self.personworkarea, thing['xyxy'])]
         notinworkareapersons = [thing for thing in activethings if
                                 thing['label'] == 'person' and not thingisinarea(self.personworkarea, thing['xyxy'])]
         notinworkareadiapers = [thing for thing in activethings if
@@ -26,6 +28,7 @@ class posture(object):
         post['count_diaper'] = len(diapers)
         post['count_inworkareaperson'] = len(inworkareapersons)
         post['count_inworkareadiaper'] = len(inworkareadiapers)
+        post['count_inpersonworkareadiapers'] = len(inpersonworkareadiapers)
         post['count_notinworkareaperson'] = len(notinworkareapersons)
         post['count_notinworkareadiaper'] = len(notinworkareadiapers)
         post['count_persontouchdiapernotinworkarea'] = getcount_persontouchobj(notinworkareapersons,
@@ -33,6 +36,7 @@ class posture(object):
         post['count_persontouchdiaperininworkarea'] = getcount_persontouchobj(inworkareapersons,
                                                                               inworkareadiapers)
         return post
+
 
 def thingisinarea(area, thing_xyxy):
     avex, avey = xyxy2avexy(thing_xyxy)
@@ -63,8 +67,89 @@ def xyxy2avexy(thing_xyxy):
     return avex, avey
 
 
+class action(object):
+    def __init__(self, window, threscount):
+        self.window = window
+        self.threscount = threscount
+
+    def takediapernotinarea(self, postdf, threscount=1):
+        col = 'count_persontouchdiapernotinworkarea'
+        windowpostdf = postdf[-self.window['takediapernotinarea']:]
+        count = (windowpostdf[col] >= threscount).sum()
+        return count >= self.threscount['takediapernotinarea']
+
+    def personinarea(self, postdf, threscount=1):
+        col = 'count_inworkareaperson'
+        windowpostdf = postdf[-self.window['personinarea']:]
+        count = (windowpostdf[col] >= threscount).sum()
+        return count >= self.threscount['personinarea']
+
+    def diaperinarea(self, postdf, threscount=1):
+        col = 'count_inworkareadiaper'
+        windowpostdf = postdf[-self.window['diaperinarea']:]
+        count = (windowpostdf[col] >= threscount).sum()
+        return count >= self.threscount['diaperinarea']
+
+    def diaperinpersonarea(self, postdf, threscount=1):
+        col = 'count_inpersonworkareadiapers'
+        windowpostdf = postdf[-self.window['diaperinarea']:]
+        count = (windowpostdf[col] >= threscount).sum()
+        return count >= self.threscount['diaperinarea']
+
+    def getaction(self, postdf):
+        action_takediapernotinarea = self.takediapernotinarea(postdf)
+        action_onepersoninarea = self.personinarea(postdf, threscount=1)
+        action_twopersoninarea = self.personinarea(postdf, threscount=2)
+        action_diaperinarea = self.diaperinarea(postdf)
+        action_diaperinpersonarea = self.diaperinpersonarea(postdf)
+        return {'action_takediapernotinarea': action_takediapernotinarea,
+                'action_onepersoninarea': action_onepersoninarea,
+                'action_twopersoninarea': action_twopersoninarea,
+                'action_diaperinarea': action_diaperinarea,
+                'action_diaperinpersonarea': action_diaperinpersonarea}
+
+
 class comboaction(object):
-    pass
+    def __init__(self, patient):
+        self.status = 'waittrigger'
+        self.patient = patient
+        self.count = 0
+
+    def update(self, action):
+        # original_status = self.status
+        if self.status == 'waittrigger':
+            if action['action_takediapernotinarea']:
+                self.status = 'takediapernotinarea'
+            else:
+                self.count += 1
+        elif self.status == 'takediapernotinarea':
+            if action['action_twopersoninarea']:
+                self.status = 'twopersoninarea'
+            else:
+                self.count += 1
+        elif self.status == 'twopersoninarea':
+            if action['action_diaperinarea']:
+                self.status = 'replacediaper'
+            else:
+                self.count += 1
+        elif self.status == 'replacediaper':
+            if action['action_takediapernotinarea'] or (
+                    not action['action_diaperinarea'] and not action['action_twopersoninarea']):
+                self.status = 'finish'
+            else:
+                self.count += 1
+        else:
+            self.status = 'waittrigger'
+            self.count = 0
+
+        if self.count >= self.patient:
+            self.status = 'waittrigger'
+            self.count = 0
+
+        # if self.status == original_status and self.status != 'waittrigger':
+        #     self.count += 1
+
+        return self.status
 
 
 class timer(object):
